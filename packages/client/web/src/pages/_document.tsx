fa5396fe23eb11490c58ad20da703b890884d6ef
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-08 18:12:03 -0300

    feat(client): adiciona fonte Inter (400,500 e 700)

--- packages/client/web/src/pages/_document.tsx
+++ packages/client/web/src/pages/_document.tsx
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
 
