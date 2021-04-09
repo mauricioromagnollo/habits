5b83804cc38f0f8d7d96171c099990e9510381d0
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-09 20:50:47 -0300

    feat: adiciona server com express
    Inicia configuração do arquivo principal do servidor, adicionando
    express, dotenv e os middlewares (cors e express.json()).

--- packages/server/src/main/server.ts
+++ packages/server/src/main/server.ts
@@ -1,1 +1,17 @@
-console.log('[*] Server!');
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+const app = express();
+
+const { SERVER_PORT } = process.env;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(SERVER_PORT, () => {
+  console.log(`[*] Server started on port ${SERVER_PORT}...`);
+});
