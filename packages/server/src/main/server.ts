304316be086dc7b206f25ff05ed23c5e144f41f8
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-13 20:29:44 -0300

    feat: adiciona rotas e middleware de erros

--- packages/server/src/main/server.ts
+++ packages/server/src/main/server.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import '../database/connection';
+
+import { AppError } from '../error/app-error';
+import { routes } from '../routes';
 
 const app = express();
 
@@ -11,6 +15,21 @@ const { SERVER_PORT } = process.env;
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`[*] Server started on port ${SERVER_PORT}...`);
