75b434e23126bee8a4cc26df27394a1970c604b5
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-13 20:26:20 -0300

    feat: adiciona SessionController

---
+++ packages/server/src/controllers/session.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+
+import { AppError } from '../error/app-error';
+import { HashProvider } from '../providers/hash';
+import { UserRepository } from '../repositories/user';
+
+export class SessionController {
+  constructor() {}
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const userRepository = new UserRepository();
+
+    const user = await userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User not founded!', 401);
+    }
+
+    const isTheSamePassword = await HashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!isTheSamePassword) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    return response.json(user);
+  }
+}
