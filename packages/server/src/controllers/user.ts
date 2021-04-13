6dc43a71f55308089972bc2137ea53adc8db68fa
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-13 20:26:07 -0300

    feat: adiciona UserController

---
+++ packages/server/src/controllers/user.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+
+import { HashProvider } from '../providers/hash';
+import { UserRepository } from '../repositories/user';
+
+export class UserController {
+  constructor() {}
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    // CreateUserService
+    const userRepository = new UserRepository();
+
+    const isAlreadyExist = await userRepository.findByEmail(email);
+
+    if (isAlreadyExist) {
+      return response.json({ message: 'Usuário já existe!' });
+    }
+
+    const hashedPassword = await HashProvider.generateHash(password);
+
+    const user = await userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return response.json(user);
+  }
+}
