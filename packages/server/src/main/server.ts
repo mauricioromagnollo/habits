7dc9e170c3440f287e622e7142e5722ec50c74eb
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-14 16:59:53 -0300

    feat: atualiza server.ts

--- packages/server/src/main/server.ts
+++ packages/server/src/main/server.ts
@@ -17,19 +17,21 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
+app.use(
+  (err: Error, _request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(SERVER_PORT, () => {
   console.log(`[*] Server started on port ${SERVER_PORT}...`);
