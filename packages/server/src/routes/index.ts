4967e793f270554e7614a0d0ca2bc78e83a1e11f
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-14 02:34:08 -0300

    feat: adiciona rotas para event

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { sessionRoutes } from './session';
 import { userRoutes } from './user';
+import { eventRoutes } from './event';
 
 const routes = Router();
 
 routes.use('/sessions', sessionRoutes);
 routes.use('/users', userRoutes);
+routes.use('/events', eventRoutes);
 
 export { routes };
