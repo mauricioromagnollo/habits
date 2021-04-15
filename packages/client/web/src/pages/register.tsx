c9ef74549422bc3f0dfb031ab5ef2e3366638005
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-15 16:24:47 -0300

    feat: adiciona página de cadastro

---
+++ packages/client/web/src/pages/register.tsx
@@ -0,0 +1,71 @@
+import { FormEvent, useState, ChangeEvent } from 'react';
+import { useRouter } from 'next/router';
+
+import api from '../services/api';
+
+import { Header } from '../components/header';
+
+import { Main, Form, Input } from '../styles/pages/register';
+
+export default function Register(): JSX.Element {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const router = useRouter();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const { name, email, password } = formData;
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('password', password);
+
+    alert('Usuário criado!');
+
+    api.post('/users', data);
+
+    router.push('/');
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            onChange={handleInputChange}
+          />
+
+          <Input
+            type="text"
+            name="email"
+            placeholder="e-mail"
+            onChange={handleInputChange}
+          />
+
+          <Input
+            type="password"
+            name="password"
+            placeholder="senha"
+            onChange={handleInputChange}
+          />
+          <button type="submit">Cadastrar</button>
+        </Form>
+      </Main>
+    </>
+  );
+}
