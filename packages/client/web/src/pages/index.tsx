d7a3f35ddb89180a8b7daefe5629d65d2fdcd6a5
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-15 03:39:15 -0300

    feat: adiciona imagem, title e checklist da página Home

--- packages/client/web/src/pages/index.tsx
+++ packages/client/web/src/pages/index.tsx
@@ -1,11 +1,39 @@
+import { BiCheck } from 'react-icons/bi';
 import { Header } from '../components/header';
-import { Hello } from '../styles/pages/home';
+import { Container, Main, Section } from '../styles/pages/home';
 
 export default function Home(): JSX.Element {
   return (
-    <>
+    <Container>
       <Header />
-      <Hello>Hello World!</Hello>
-    </>
+      <Main>
+        <Section>
+          <h1>Calendário de Eventos</h1>
+          <h3>
+            Uma forma símples e fácil de registrar e gerenciar os seus eventos
+            importantes!
+          </h3>
+          <ul>
+            <div>
+              <BiCheck color="#FB6404" size={30} />
+              <li>Crie seus eventos</li>
+            </div>
+            <div>
+              <BiCheck color="#FB6404" size={30} />
+              <li>Convide seus amigos</li>
+            </div>
+            <div>
+              <BiCheck color="#FB6404" size={30} />
+              <li>Não perca nenhum evento importante</li>
+            </div>
+            <div>
+              <BiCheck color="#FB6404" size={30} />
+              <li>Veja os eventos do mês</li>
+            </div>
+          </ul>
+        </Section>
+        <img src="calendar.svg" alt="Calendário" />
+      </Main>
+    </Container>
   );
 }
--- packages/client/web/src/styles/pages/home.ts
+++ packages/client/web/src/styles/pages/home.ts
@@ -1,5 +1,57 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Hello = styled.h1`
-  color: var(--gray);
+export const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3px;
+
+  > img {
+    width: 100%;
+    max-width: 600px;
+    margin: 8rem 2rem 0 0;
+  }
+`;
+
+export const Section = styled.section`
+  margin: 0 0 0 50px;
+
+  h1,
+  h3,
+  ul {
+    margin-top: 30px;
+  }
+
+  > h1 {
+    color: var(--dark);
+    font-weight: 700;
+    font-size: 2.8rem;
+  }
+
+  > h3 {
+    color: var(--gray);
+    max-width: 30rem;
+    text-align: justify;
+    font-weight: 400;
+  }
+
+  ul {
+    list-style: none;
+
+    li {
+      color: var(--blue);
+      font-weight: 700;
+      margin-left: 10px;
+    }
+
+    > div {
+      display: flex;
+      align-items: center;
+    }
+  }
 `;
