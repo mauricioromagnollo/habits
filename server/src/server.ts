757358bdee533685e7221b3608a738ec8dc67157
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-16 22:38:12 -0300

    chore(server): setup server app with Fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,20 @@
-console.log('test')
+import { PrismaClient } from "@prisma/client"
+import Fastify from "fastify"
+import cors from '@fastify/cors'
+
+const app = Fastify()
+app.register(cors, {
+  origin: '*'
+})
+
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 3333
+})
