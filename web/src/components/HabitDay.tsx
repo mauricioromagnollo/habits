e98255f079d45f97c45365bd86c9fbcecb161cb4
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-20 00:49:10 -0300

    feat(web): add color intensity by completed percentage

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,43 @@
+import * as Popover from "@radix-ui/react-popover"
+import clsx from 'clsx'
+import { ProgressBar } from "./ProgressBar"
+
 interface HabitDayProps {
-  filled?: boolean
+  disabled?: boolean
+  completed?: number
+  amount?: number
 }
 
-export function HabitDay({ filled = true }: HabitDayProps) {
-  if (!filled) {
-    return <div className="w-10 h10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+export function HabitDay({ disabled = false, completed = 0, amount = 1 }: HabitDayProps) {
+  const commonClass = 'w-10 h10 bg-zinc-900 border-2 border-zinc-800 rounded-lg'
+
+  if (disabled) {
+    return <div className={`${commonClass} opacity-40 cursor-not-allowed`}></div>
   }
 
-  return <div className="w-10 h10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  const completedPercentage = Math.round((completed / amount) * 100)
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger
+        className={clsx(commonClass, {
+          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+          'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+          'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+          'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+          'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+          'bg-violet-500 border-violet-400': completedPercentage >= 80
+        })}
+      />
+      <Popover.Portal>
+        <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
+
+          <ProgressBar progress={completedPercentage} />
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  )
 }
